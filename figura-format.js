9ab83d6a807942d01f8d0ac26cf4748437799f18
KitCat962 <kitcat0962@gmail.com>
2023-06-14 17:19:26 -0400

    group name limitation yeeted

--- figura-format.js
+++ figura-format.js
@@ -51,6 +51,9 @@
                 onDeactivation() {
                 }
             })
+
+            Group.prototype.name_regex = () => Format.id=="figura" ? false : Format.bone_rig ? 'a-zA-Z0-9_' : false;
+            Group.prototype.needsUniqueName = () => Format.id=="figura" ? false : Format.bone_rig;
         },
         onunload() {
             modelFormat.delete()
