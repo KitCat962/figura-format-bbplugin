677d8cce5c3761afd59eb9aee3f2b89a3a347808
KitCat962 <kitcat0962@gmail.com>
2023-07-22 16:57:00 -0400

    made some intrusive changes more flexible to other plugins

--- figura-format.js
+++ figura-format.js
@@ -80,13 +80,15 @@
                     Canvas.updateView({ elements: Mesh.selected, element_aspects: { geometry: true, uv: true, faces: true } });
                 }
             }))
-
-            Group.prototype.name_regex = () => Format === format? false : Format.bone_rig ? 'a-zA-Z0-9_' : false;
-            Group.prototype.needsUniqueName = () => Format === format ? false : Format.bone_rig;
+            let name_regex = Group.prototype.name_regex, needsUniqueName = Group.prototype.needsUniqueName
+            Group.prototype.name_regex = () => Format === format ? false : name_regex();
+            Group.prototype.needsUniqueName = () => Format === format ? false : needsUniqueName();
 
             let molangSyntax = Validator.checks.find(element => element.id == 'molang_syntax')
-            if (molangSyntax)
-                molangSyntax.condition = () => Format === format ? false : Format.animation_mode
+            if (molangSyntax) {
+                let method = molangSyntax.condition.method
+                molangSyntax.condition.method = (context) => Format === format ? false : (method ? method(context) : false)
+            }
 
             let showMessageBox = Blockbench.showMessageBox
             Blockbench.showMessageBox = function (options, callback) {
